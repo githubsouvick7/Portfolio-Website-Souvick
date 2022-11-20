d4aefd1a14901c772d1a3587951e28f6e0de8f88
Souvick Chakraborty <114563269+githubsouvick7@users.noreply.github.com>
2022-11-20 12:48:32 +0530

    Add files via upload

---
+++ portfolio/public/netflix/index.js
@@ -0,0 +1,21 @@
+function toggleHide(){
+    let two = document.getElementById("two");
+    let para = document.getElementById('para');
+    if(para.style.display != 'none'){
+        para.style.display = 'none';
+    }
+    else{
+        para.style.display = 'block';
+    }
+}
+
+function toggleHidden() {
+    let three = document.getElementById('three');
+    let paraone = document.getElementById('paraone');
+    if(paraone.style.display != 'none'){
+        paraone.style.display = 'none';
+    }
+    else{
+        paraone.style.display = 'block';
+    }
+}
